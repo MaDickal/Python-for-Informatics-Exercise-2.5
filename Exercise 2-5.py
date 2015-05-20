f2b95adbc2c6a46f115f702cc6785b4c45d3f1c0
Matt Dickal <matt.dickal@gmail.com>
2015-05-20 12:09:59 -0700

    Exercise 2.5

---
+++ Exercise 2-5.py
@@ -0,0 +1,7 @@
+# Celsius to Fahrenheit conversion
+
+temp = raw_input('Enter the temperature in degrees Celsius: ')
+celsius = float(temp)
+fahrenheit = celsius * 1.8 + 32
+fahrenheit = str(fahrenheit)
+print temp + ' degrees Celsius is equal to ' + fahrenheit + ' degrees Fahrenheit'
